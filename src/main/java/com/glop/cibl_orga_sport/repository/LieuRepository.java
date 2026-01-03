fc16ec79e1851dd2683a1c0e47b724cd07955109
CyrilGodet <cyril.godet.etu@univ-lille.fr>
2026-01-03 12:54:46 +0100

    ajout LieuRepository

---
+++ src/main/java/com/glop/cibl_orga_sport/repository/LieuRepository.java
@@ -0,0 +1,7 @@
+package com.glop.cibl_orga_sport.repository;
+
+import com.glop.cibl_orga_sport.data.Lieu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LieuRepository extends JpaRepository<Lieu, Long> {
+}

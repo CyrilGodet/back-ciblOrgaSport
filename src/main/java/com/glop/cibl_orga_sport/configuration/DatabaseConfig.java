ac0f22f6dd5da3fe92fd049e78ff6ca95dc65409
CyrilGodet <cyril.godet.etu@univ-lille.fr>
2026-03-06 20:20:45 +0100

    corection url bdd

---
+++ src/main/java/com/glop/cibl_orga_sport/configuration/DatabaseConfig.java
@@ -0,0 +1,47 @@
+package com.glop.cibl_orga_sport.configuration;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@Profile("prod")
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        
+        if (databaseUrl == null || databaseUrl.isEmpty()) {
+            throw new IllegalStateException("DATABASE_URL environment variable is not set");
+        }
+
+        try {
+            URI dbUri = new URI(databaseUrl);
+            
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String host = dbUri.getHost();
+            int port = dbUri.getPort();
+            String database = dbUri.getPath().substring(1); // Remove leading '/'
+            
+            String jdbcUrl = String.format("jdbc:postgresql://%s:%d/%s", host, port, database);
+            
+            return DataSourceBuilder
+                    .create()
+                    .url(jdbcUrl)
+                    .username(username)
+                    .password(password)
+                    .driverClassName("org.postgresql.Driver")
+                    .build();
+                    
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("Invalid DATABASE_URL format", e);
+        }
+    }
+}

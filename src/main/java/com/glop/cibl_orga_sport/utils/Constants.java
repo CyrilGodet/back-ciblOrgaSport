9468363dd38022625c936f12ac9af602e116ad07
s-mgy <maggyandriambelomahery@gmail.com>
2026-03-14 22:31:00 +0100

    exception & utils & swagger

---
+++ src/main/java/com/glop/cibl_orga_sport/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.glop.cibl_orga_sport.utils;
+
+public interface Constants {
+    String APP_NAME = "opaleTech";
+    String ROLES_ENDPOINT = APP_NAME + "/roles";
+
+    String USER_ENDPOINT = APP_NAME + "/users";
+
+    String ROLE_ENDPOINT = APP_NAME + "/role";
+
+    String LOGIN_ENDPOINT = APP_NAME + "/login";
+
+    String MDP_CHANGE_ENDPOINT = APP_NAME +"/users/password";
+}
